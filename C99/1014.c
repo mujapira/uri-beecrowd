98dfe9b87546986bd9a66a545269270e84cb98fc
Mujapira <malmasmello2@gmail.com>
2023-02-26 19:59:46 -0300

    1014.c fix

--- C99/1014.c
+++ C99/1014.c
@@ -4,9 +4,9 @@ int main() {
     float consumption, fuelSpent;
     int distance;
     
-    consumption = distance / fuelSpent;
 
-    scanf("%d %f %f", &distance, &consumption, &fuelSpent);
+    scanf("%d %f %f", &distance, &fuelSpent);
+    consumption = distance / fuelSpent;
     
     printf("%.3f km/l\n", consumption);
 
