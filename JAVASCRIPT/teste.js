e53a8c07d10902b92408fcedbaed23b02ff5dbb3
Mujapira <malmasmello2@gmail.com>
2023-02-26 19:52:50 -0300

    js test and 1035.c

--- JAVASCRIPT/teste.js
+++ JAVASCRIPT/teste.js
@@ -1,8 +1,46 @@
-"use strict"
-var input = require('fs').readFileSync('./input.txt', 'utf8');
-var lines = input.split('\n');
+"use strict";
+var input = require("fs").readFileSync("./input.txt", "utf8");
+var lines = input.split("\n");
 
 const a = Number(lines[0]);
+
+// numerador = cima
+
 for (let i = 1; i <= a; i++) {
-    console.log(`${i} ${i*i} ${i*i*i}\n${i} ${i*i+1} ${i*i*i+1}`)
+  let newArray = lines[i].split(" ");
+  let cima1 = newArray[0];
+  let baixo1 = newArray[2];
+  let sinal = newArray[3];
+  let cima2 = newArray[4];
+  let baixo2 = newArray[6];
+
+  let result = 0;
+  let frac = [];
+
+  if (sinal === "+") {
+    result = cima1 / baixo1 + cima2 / baixo2;
+    frac = [cima1 * baixo2 + baixo1 * cima2, baixo1 * baixo2];
+  }
+  if (sinal === "-") {
+    result = cima1 / baixo1 - cima2 / baixo2;
+    frac = [cima1 * baixo2 - baixo1 * cima2, baixo1 * baixo2];
+  }
+
+  if (sinal === "*") {
+    result = ((cima1 / baixo1) * cima2) / baixo2;
+    frac = [cima1 * cima2, baixo1 * baixo2];
+  }
+  if (sinal === "/") {
+    result = cima1 / baixo1 / (cima2 / baixo2);
+    frac = [cima1 * baixo2, baixo1 * cima2];
+  }
+  
+  function getFraction(decimal) {
+    for (var denominador = 1; (decimal * denominador) % 1 !== 0; denominador++);
+    return [decimal * denominador, denominador];
+  }
+
+  let fracSimples = getFraction(result);
+
+  console.log(`${frac[0]} / ${frac[1]} = ${fracSimples[0]} / ${fracSimples[1]}`);
 }
