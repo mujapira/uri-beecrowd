c3ca9416dd01138d79696345cb1fa12cba7730c1
Mujapira <malmasmello2@gmail.com>
2023-02-26 18:29:20 -0300

    js config

---
+++ JAVASCRIPT/teste.js
@@ -0,0 +1,8 @@
+"use strict"
+var input = require('fs').readFileSync('./input.txt', 'utf8');
+var lines = input.split('\n');
+
+const a = Number(lines[0]);
+for (let i = 1; i <= a; i++) {
+    console.log(`${i} ${i*i} ${i*i*i}\n${i} ${i*i+1} ${i*i*i+1}`)
+}
