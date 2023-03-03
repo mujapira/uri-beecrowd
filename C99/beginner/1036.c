fe200f3099b2500b7d095ddd09933b2978f9aec7
Mujapira <malmasmello2@gmail.com>
2023-03-03 15:22:44 -0300

    1035 - 1037

---
+++ C99/beginner/1036.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  double a, b, c, delta, x1, x2;
+  scanf("%lf %lf %lf", &a, &b, &c);
+
+  delta = b * b - 4 * a * c;
+
+  if (a == 0) {
+    printf("Impossivel calcular\n");
+  } else {
+    if (delta > 0) {
+      x1 = (-b + pow(delta, 0.5)) / (2 * a);
+      x2 = (-b - pow(delta, 0.5)) / (2 * a);
+      printf("R1 = %.5lf\n", x1);
+      printf("R2 = %.5lf\n", x2);
+
+    } else {
+      printf("Impossivel calcular\n");
+    }
+  }
+  return 0;
+}
